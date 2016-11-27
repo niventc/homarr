a85101faf41abad8086443a536a13d8ce2d17d73
Vincent Sparkes <Vincent Sparkes>
2016-11-27 18:06:40 +0000

    Hacky material input...

--- src/homarr-shopping/src/app/app.component.ts
+++ src/homarr-shopping/src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Component, trigger, animate, transition, style } from '@angular/core';
     trigger('fadeInOut', [
       transition(':enter', [
         style({transform: 'scale(0)', 'transform-origin': 'right bottom'}),
-        animate(200, style({transform: 'scale(1)', 'transform-origin': 'right bottom'}))
+        animate('200ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({transform: 'scale(1)', 'transform-origin': 'right bottom'}))
       ]),
       transition(':leave', [
-        animate(200, style({transform: 'scale(0)', 'transform-origin': 'right bottom'}))
+        animate('200ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({transform: 'scale(0)', 'transform-origin': 'right bottom'}))
       ])
     ])
   ]
